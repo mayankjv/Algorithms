dcca76b3160efcbd687d2cbb9032b635943a976b
Mayank Patel <mayank.patel@jec-jabalpur.org>
2017-09-14 20:27:22 +0530

    Rabin-Karp Algorithm
    This is one of the algorithms for string matching. This algorithm takes O(M*N) time in worst case where M and N are the lengths of the given Text and Pattern.

---
+++ RabinKarp.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class RabinKarp{
+    public static void main(String args[]){
+        String text="abcbcglx";
+        String pattern="bcf";
+        int m=text.length();
+        int n=pattern.length();
+        int hashOfPattern=calculateHash(pattern);
+        //System.out.println("Hash of Pattern:"+hashOfPattern);
+        for(int i=0;i<=(m-n);i++){
+            int hs=calculateHash(text.substring(i,i+n));
+            //System.out.println("Hash"+i+":"+hs);
+            if(hs==hashOfPattern){
+                if((text.substring(i,i+n)).compareTo(pattern)==0)
+                    i++;
+                    System.out.println("Found at Position:"+i);
+                    return;
+            }
+        }
+        System.out.println("Pattern Not Found!");
+    }
+    public static int calculateHash(String s){
+        char[] ch= s.toCharArray();
+        int prime=29;
+        int hash=0;
+        for(int i=0;i<ch.length;i++){
+            hash+=ch[i]*Math.pow(prime, i);
+        }
+        return hash;
+    }
+}
