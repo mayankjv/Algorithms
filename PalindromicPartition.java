d9341cffdf52501ba89e53d46480b5953b4a1c1d
Mayank Patel <mayank.patel@jec-jabalpur.org>
2017-09-23 17:32:12 +0530

    Palindromic Partition 2
    Updated Problem description.

--- PalindromicPartition.java
+++ PalindromicPartition.java
@@ -1,3 +1,9 @@
+/*
+* This is one of the standard problems of Dynamic Programming, in which we are given with on string 
+* and we have to find the minimum number of partitions such that all the resulting strings are Palindromes.
+*/
+import java.util.*;
+
 class PalindromicPartition {
     public static int minCut(String A) {
         int n=A.length();
