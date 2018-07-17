1fbc634d087eba578517f0c542bf2ba0c6d52036
Mayank Patel <mayank.patel@jec-jabalpur.org>
2018-07-17 06:50:53 +0000

    Update encrypt.c

--- Assign/encrypt.c
+++ Assign/encrypt.c
@@ -2,20 +2,34 @@
 */
 #include<stdio.h>
 
-int encrypt(int num); //function declaration of the encryption function
-int decrypt(int num); //function declaration of the encryption function
+long encrypt2(long num);
+long encrypt(long num); //function declaration of the encryption function
+long decrypt(long num); //function declaration of the encryption function
+long decrypt2(long num);
 int main(){
-	int num; // a variable to take input from the user
-	scanf("%d",&num); // getting the integer input
-	printf("The Encrypted number is: %d\n", encrypt(num));  //printing the encrptyed number
-	printf("The Decrypted number is: %d\n", decrypt(encrypt(num)));  //printing the decrypted number
+	long num; // a variable to take input from the user
+	scanf("%ld",&num); // getting the integer input
+	printf("The Encrypted number using algorithm 1 is: %ld\n", encrypt(num));  //printing the encrptyed number
+	printf("The Encrypted number using algorithm 2 is: %ld\n", encrypt2(num));  //printing the encrptyed number
+	printf("The Decrypted number using algorithm 1 is: %ld\n", decrypt(encrypt(num)));  //printing the decrypted number
+	printf("The Decrypted number using algorithm 2 is: %ld\n", decrypt2(encrypt2(num)));  //printing the decrypted number
 	return 0;
 }
 // a fuction that accepts a number and returns after encrypting it
-int encrypt(int num){
+long encrypt(long num){
 	return ~num; // Inverting all the bits of the number to change its bit pattern
 }
+long encrypt2(long num){
+	long temp=1;
+	temp=temp<<8;
+	return temp^num;
+}
+long decrypt2(long num){
+	long temp=1;
+	temp=temp<<8;
+	return temp^num;
+}
 // a function that accepts encrypted number and returns after decrypting it
-int decrypt(int num){
+long decrypt(long num){
 	return ~num; // inverting all the bits of encrypted number to get the original number back
 }
