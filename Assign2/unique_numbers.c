4a9cd4d010b3f55a5d104542258e09e7963fac17
Mayank Patel <mayank.patel@jec-jabalpur.org>
2018-07-18 06:18:50 +0000

    Update unique_numbers.c

--- Assign2/unique_numbers.c
+++ Assign2/unique_numbers.c
@@ -14,9 +14,13 @@ int main(){
 	int *arr= (int*)malloc(size*sizeof(int));  //dynamic memory allocation to the array
 	printf("Enter the elements of the array:");
 	//Taking input
-	for(int i=0;i<size;i++)
+	//printf("Size: %d",size);  Debug statement
+	for(int i=0;i<size;i++){
 		scanf("%d",&arr[i]);
+		//printf("scanning %d\n",i+1); Debug statement
+	}
 	quicksort(arr,0,size-1); //Function call to quicksort
+	//printf("\nQuicksort Done !\n"); Debug Statement
 	for(int i=0;i<size;){
 		if(arr[i]==arr[i+1]){  //checking if the currect element is equal to the next element
 			int temp=arr[i];
@@ -31,16 +35,18 @@ int main(){
 }
 //recursive function for quicksort, which takes an array as input and the boundary indices which are to be sorted
 void quicksort(int arr[], int start, int end){
-	int pivot= start; // taking the first element as the pivot everytime quicksort is called
-	int first=start; //this will be the running variable that will store the left bound during traversal
-	int last=end;   //this will be the running variable that will store the right bound during traversal
-	while(first<last){
-		while(arr[first]<=arr[pivot] && first<last) //skip all the elements starting at the beginning that are smaller than or equal to the pivot element
-			first++;
-		while(arr[last]>arr[pivot] )  //skip all the elements starting at the end that are greater than pivot element
-			last--;
-		if(first<last){
-			swap(arr, first ,last); //swap the elements that first and the last index because the at this point of time the element at last is smaller than pivot and the one at first is larger than pivot element
+	if(start<end){
+		int pivot= start; // taking the first element as the pivot everytime quicksort is called
+		int first=start; //this will be the running variable that will store the left bound during traversal
+		int last=end;   //this will be the running variable that will store the right bound during traversal
+		while(first<last){
+			while(arr[first]<=arr[pivot] && first<end) //skip all the elements starting at the beginning that are smaller than or equal to the pivot element
+				first++;
+			while(arr[last]>arr[pivot] )  //skip all the elements starting at the end that are greater than pivot element
+				last--;
+			if(first<last){
+				swap(arr, first ,last); //swap the elements that first and the last index because the at this point of time the element at last is smaller than pivot and the one at first is larger than pivot element
+			}
 		}
 		swap(arr,pivot,last); // swap in order to send pivot to its position
 		quicksort(arr,start,last-1); //call quicksort for the left side of pivot elemet
