2e29503ab62f3a0414da787f0ed6a6c1e68eb0f8
Mayank Patel <mayank.patel@jec-jabalpur.org>
2018-07-18 07:57:24 +0530

    Update unique_numbers.c

--- Assign2/unique_numbers.c
+++ Assign2/unique_numbers.c
@@ -1,7 +1,10 @@
-
+//This program takes the size of an array and the array as input from the user and
+//displays only the elements that occur just once in the array.
+// the approach used is to sort the elements and if any two consecutive elements are equal, do not print them.
 #include<stdio.h>
 #include<stdlib.h>
 
+//Quicksort algorithm to sort the array
 void quicksort(int *arr,int first,int last){
    int i, j, pivot, temp;
 
@@ -34,18 +37,19 @@ int main(){
 	int size;
 	printf("Enter the size of the array:");
 	scanf("%d",&size);
-	int *arr= (int*)malloc(size*sizeof(int));
+	int *arr= (int*)malloc(size*sizeof(int));  //dynamic memory allocation to the array
 	printf("Enter the elements of the array:");
+	//Taking input
 	for(int i=0;i<size;i++)
 		scanf("%d",&arr[i]);
-	quicksort(arr,0,size-1);
+	quicksort(arr,0,size-1); //Function call to quicksort
 	for(int i=0;i<size;){
-		if(arr[i]==arr[i+1]){
+		if(arr[i]==arr[i+1]){  //checking if the currect element is equal to the next element
 			int temp=arr[i];
-			while(arr[i]==temp && i<size)
+			while(arr[i]==temp && i<size)	//skipping all the elements that are equal to the current element
 				i++;
 		}
-		else{
+		else{ //If the two adjacent elements are not equal then print the current element.
 			printf("%d ",arr[i]);
 			i++;
 		}
