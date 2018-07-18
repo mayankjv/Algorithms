e04f802c8702b429fe3c77892836e09b0db03f69
Mayank Patel <mayank.patel@jec-jabalpur.org>
2018-07-18 07:13:54 +0000

    Update nth_largest.c

--- Assign2/nth_largest.c
+++ Assign2/nth_largest.c
@@ -8,8 +8,54 @@ the algorithm used for the same is a slight optimisation of heap sort */
 //function declarations
 void Heapify(int a[],int size, int curr); 
 void swap(int arr[], int idx1, int idx2);
+void quicksort(int arr[], int start, int end);
 void buildMinHeap(int arr[],int size);
 int nthlargest(int arr[],int size,int n);
+int remove_duplicates(int arr[], int size);
+
+int main(){
+    int size,n;
+    printf("Enter the size of the array:");
+    scanf("%d",&size);
+    int *arr= (int*)malloc(size*sizeof(int));  //dynamic memory allocation to the array
+    printf("Enter the elements of the array:");
+    //Taking input
+    for(int i=0;i<size;i++)
+	scanf("%d",&arr[i]);
+    quicksort(arr,0,size-1);
+    int size_new=remove_duplicates(arr, size);
+    printf("Enter the values of N:");
+    scanf("%d",&n);
+    //printf("New Size=%d",size_new); debug statement
+    if(size_new<n){
+	printf("There are few unique numbers in the array!");
+    }
+    else
+    printf("The required number is: %d\n",nthLargest(arr,size_new,n));
+
+}
+
+
+
+int remove_duplicates(int arr[], int size){
+	int k=0;    	
+	for(int i=0;i<size;){
+		if(i==size-1){
+			arr[k++]=arr[i++];
+			continue;
+		}
+		else if(arr[i]==arr[i+1]){  //checking if the currect element is equal to the next element
+			int temp=arr[i];
+			while(arr[i]==temp && i<size)	//skipping all the elements that are equal to the current element
+				i++;
+			arr[k++]=temp;	
+		}
+		else{ //If the two adjacent elements are not equal then print the current element.
+			arr[k++]=arr[i++];
+		}
+	}
+	return k;
+}
 
 //function takes an array and two indexes as input and swaps the numbers at the two indexes.
 void swap(int arr[], int idx1, int idx2){
@@ -65,17 +111,30 @@ int nthLargest(int arr[], int size, int n)
     }
     return minHeap[0];
 }
-int main(){
-    int size,n;
-    printf("Enter the size of the array:");
-    scanf("%d",&size);
-    int *arr= (int*)malloc(size*sizeof(int));  //dynamic memory allocation to the array
-    printf("Enter the elements of the array:");
-    //Taking input
-    for(int i=0;i<size;i++)
-        scanf("%d",&arr[i]);
-    printf("Enter the values of N:");
-    scanf("%d",&n);
-    printf("The required number is: %d\n",nthLargest(arr,size,n));
 
+
+
+
+
+
+//recursive function for quicksort, which takes an array as input and the boundary indices which are to be sorted
+void quicksort(int arr[], int start, int end){
+	if(start<end){
+		int pivot= start; // taking the first element as the pivot everytime quicksort is called
+		int first=start; //this will be the running variable that will store the left bound during traversal
+		int last=end;   //this will be the running variable that will store the right bound during traversal
+		while(first<last){
+			while(arr[first]<=arr[pivot] && first<end) //skip all the elements starting at the beginning that are smaller than or equal to the pivot element
+				first++;
+			while(arr[last]>arr[pivot] )  //skip all the elements starting at the end that are greater than pivot element
+				last--;
+			if(first<last){
+				swap(arr, first ,last); //swap the elements that first and the last index because the at this point of time the element at last is smaller than pivot and the one at first is larger than pivot element
+			}
+		}
+		swap(arr,pivot,last); // swap in order to send pivot to its position
+		quicksort(arr,start,last-1); //call quicksort for the left side of pivot elemet
+		quicksort(arr,last+1,end); //call quicksort for the right side of the pivot element.
+		
+	}
 }
