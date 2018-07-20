2eb17bc76f670d0e046be2272330d59acc27ae79
Mayank Patel <mayank.patel@jec-jabalpur.org>
2018-07-20 04:13:28 +0000

    Update encrypt.c

--- Assign/encrypt.c
+++ Assign/encrypt.c
@@ -1,35 +1,41 @@
 /* This program encrypts a given number using the binary inverter operator.
 */
 #include<stdio.h>
+#include<stdlib.h>
 
-long encrypt2(long num);
-long encrypt(long num); //function declaration of the encryption function
-long decrypt(long num); //function declaration of the encryption function
-long decrypt2(long num);
-int main(){
-	long num; // a variable to take input from the user
-	scanf("%ld",&num); // getting the integer input
-	printf("The Encrypted number using algorithm 1 is: %ld\n", encrypt(num));  //printing the encrptyed number
-	printf("The Encrypted number using algorithm 2 is: %ld\n", encrypt2(num));  //printing the encrptyed number
-	printf("The Decrypted number using algorithm 1 is: %ld\n", decrypt(encrypt(num)));  //printing the decrypted number
-	printf("The Decrypted number using algorithm 2 is: %ld\n", decrypt2(encrypt2(num)));  //printing the decrypted number
-	return 0;
+char* encrypt(char* num); //function declaration of the encryption function
+char* decrypt(char* num); //function declaration of the encryption function
+
+int main(int argc,const char* argv[]){
+	char* str= (char*)malloc(1000*sizeof(char));
+	gets(str);
+	if(argc==1){
+		printf("The Encrypted number is: %s\n", encrypt(str));  //printing the encrptyed number
+		printf("The Decrypted number is: %s\n", decrypt(encrypt(str)));  //printing the decrypted number
+		return 0;
+	}
+	else{
+		if(argv[1][0]=='0'){
+			printf("The Encrypted number is: %s\n", encrypt(str));  //printing the encrptyed number
+		}
+		else{
+			printf("The Decrypted number is: %s\n", decrypt(str));  //printing the decrypted number
+		}
+	}
 }
 // a fuction that accepts a number and returns after encrypting it
-long encrypt(long num){
-	return ~num; // Inverting all the bits of the number to change its bit pattern
-}
-long encrypt2(long num){
-	long temp=1;
-	temp=temp<<8;
-	return temp^num;
-}
-long decrypt2(long num){
-	long temp=1;
-	temp=temp<<8;
-	return temp^num;
+char* encrypt(char* str){
+//	return ~num; // Inverting all the bits of the number to change its bit pattern
+
+	char key='A';
+	char* encrypted=str;
+	for(int i=0;str[i]!='\0';i++){
+		encrypted[i]=str[i]^key;
+	}
+	return encrypted;
+
 }
 // a function that accepts encrypted number and returns after decrypting it
-long decrypt(long num){
-	return ~num; // inverting all the bits of encrypted number to get the original number back
+char* decrypt(char* str){
+	return encrypt(str); // inverting all the bits of encrypted number to get the original number back
 }
